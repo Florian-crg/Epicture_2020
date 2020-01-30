ce9b5b51e3ad0e9fd610db1ad6010364ddca1cff
florian <florian.Cargouet@epitech.eu>
2020-01-30 14:33:52 +0100

    fetching data soon

--- app/src/main/java/com/example/epicture/MainActivity.java
+++ app/src/main/java/com/example/epicture/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import okhttp3.OkHttpClient;
+
 public class MainActivity extends AppCompatActivity {
+    private OkHttpClient httpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fetchData();
     }
+
+    private void fetchData() {
+        httpClient = new OkHttpClient.Builder().build();
+    }
+
 }
