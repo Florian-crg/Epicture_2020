1b172435d73a90c85378b787544d87d10f1dc3c0
CalimReo <thibaultvandamme@hotmail.fr>
2020-01-30 12:51:57 +0100

    new activity for galery

---
+++ app/src/main/java/com/example/epicture/GaleryActivity.java
@@ -0,0 +1,14 @@
+package com.example.epicture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GaleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galery);
+    }
+}
