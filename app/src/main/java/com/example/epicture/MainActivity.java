0453b696035a7090af6ac45e0bc6c4ee8031995e
CalimReo <thibaultvandamme@hotmail.fr>
2020-01-31 10:08:45 +0100

    fragment activity with home favorites and search pages

--- app/src/main/java/com/example/epicture/MainActivity.java
+++ app/src/main/java/com/example/epicture/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.epicture;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 //import okhttp3.OkHttpClient;
 
@@ -12,10 +17,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_galery);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 //        fetchData();
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+           new BottomNavigationView.OnNavigationItemSelectedListener() {
+               @Override
+               public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                   Fragment selectedFragment = null;
+
+                   switch (item.getItemId()) {
+                       case R.id.nav_home:
+                           selectedFragment = new HomeFragment();
+                           break;
+                       case R.id.nav_favorites:
+                           selectedFragment = new FavoritesFragment();
+                           break;
+                       case R.id.nav_search:
+                           selectedFragment = new SearchFragment();
+                           break;
+                   }
+                   getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                           selectedFragment).commit();
+
+                   return true;
+               }
+           };
 //    private void fetchData() {
 //        httpClient = new OkHttpClient.Builder().build();
 //    }
