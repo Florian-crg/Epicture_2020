8b187f0737117b094aa3e0a148b64e3fdeb24a29
florian <florian.Cargouet@epitech.eu>
2020-01-31 18:37:22 +0100

    - old test viewimages

--- app/src/main/java/com/example/epicture/LoginActivity.java
+++ app/src/main/java/com/example/epicture/LoginActivity.java
@@ -1,21 +1,11 @@
 package com.example.epicture;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
-import android.graphics.Rect;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.view.MenuItem;
-import android.widget.TextView;
+
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -25,12 +15,10 @@ import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -38,30 +26,12 @@ import org.json.JSONObject;
 import java.util.Arrays;
 
 import de.hdodenhof.circleimageview.CircleImageView;
-import okhttp3.OkHttpClient;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.squareup.picasso.Picasso;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Request;
-import okhttp3.Response;
-
-//import okhttp3.OkHttpClient;
 
 public class LoginActivity extends AppCompatActivity {
-//    private OkHttpClient httpClient;
     private CallbackManager callbackManager;
     private LoginButton loginButton;
     private TextView txtName, txtEmail;
     private CircleImageView circleImageView;
-    private OkHttpClient httpClient;
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -131,10 +101,7 @@ public class LoginActivity extends AppCompatActivity {
                     txtName.setText(first_name +" "+last_name);
                     RequestOptions requestOptions = new RequestOptions();
                     requestOptions.dontAnimate();
-
                     Glide.with(LoginActivity.this).load(image_url).into(circleImageView);
-
-
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -149,104 +116,4 @@ public class LoginActivity extends AppCompatActivity {
         request.setParameters(parameters);
         request.executeAsync();
     }
-
-    private void fetchData() {
-        httpClient = new OkHttpClient.Builder().build();
-        Request request = new Request.Builder()
-                .url("https://api.imgur.com/3/gallery/user/rising/0.json")
-                .header("Authorization","Client-ID bb0c749c6403fd2")
-                .header("User-Agent","epicture")
-                .build();
-
-        httpClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.e(TAG, "An error has occurred " + e);
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    JSONObject data = new JSONObject(response.body().string());
-                    JSONArray items = data.getJSONArray("data");
-                    final List<Photo> photos = new ArrayList<Photo>();
-                    for(int i=0; i<items.length();i++) {
-                        JSONObject item = items.getJSONObject(i);
-                        Photo photo = new Photo();
-                        if(item.getBoolean("is_album")) {
-                            photo.id = item.getString("cover");
-                        } else {
-                            photo.id = item.getString("id");
-                        }
-                        photo.title = item.getString("title");
-                        photos.add(photo);
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                render(photos);
-                            }
-                        });
-                    }
-                }
-                catch (Exception e) {
-                    Log.e("JSONerr" , "Something went wrong.");
-                }
-            }
-        });
-    }
-
-    private static class PhotoVH extends RecyclerView.ViewHolder {
-        ImageView photo;
-        TextView title;
-
-        public PhotoVH(View itemView) {
-            super(itemView);
-        }
-    }
-
-    private void render(final List<Photo> photos) {
-        RecyclerView rv = (RecyclerView)findViewById(R.id.rv_of_photos);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-
-        RecyclerView.Adapter<PhotoVH> adapter = new RecyclerView.Adapter<PhotoVH>() {
-            @NonNull
-            @Override
-            public PhotoVH onCreateViewHolder(ViewGroup parent, int viewType) {
-                PhotoVH vh = new PhotoVH(getLayoutInflater().inflate(R.layout.item, null));
-                vh.photo = (ImageView) vh.itemView.findViewById(R.id.photo);
-                vh.title = (TextView) vh.itemView.findViewById(R.id.title);
-                return vh;
-            }
-
-            @Override
-            public void onBindViewHolder(PhotoVH holder, int position) {
-                Picasso.with(LoginActivity.this).load("https://i.imgur.com/" +
-                        photos.get(position).id + ".jpg").into(holder.photo);
-                holder.title.setText(photos.get(position).title);
-            }
-
-            @Override
-            public int getItemCount() {
-                return photos.size();
-            }
-        };
-
-        rv.setAdapter(adapter);
-
-//        rv.addItemDecoration(new RecyclerView.ItemDecoration() {
-//            @Override
-//            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-//                outRect.bottom = 16; // Gap of 16px
-//            }
-//        });
-    }
-
-
-    private static class Photo {
-        String id;
-        String title;
-
-    }
-
 }
