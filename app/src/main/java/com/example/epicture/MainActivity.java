9be502f90546517a43e2c48d1b71871cf6b536cd
florian <florian.Cargouet@epitech.eu>
2020-01-31 09:09:45 +0100

    In progress Api imgur get One image

--- app/src/main/java/com/example/epicture/MainActivity.java
+++ app/src/main/java/com/example/epicture/MainActivity.java
@@ -3,21 +3,53 @@ package com.example.epicture;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-//import okhttp3.OkHttpClient;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-//    private OkHttpClient httpClient;
+    private OkHttpClient httpClient;
+    private static final String TAG = "MainActivity";
+    //private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        fetchData();
+        fetchData();
     }
 
-//    private void fetchData() {
-//        httpClient = new OkHttpClient.Builder().build();
-//    }
+    private void fetchData() {
+        httpClient = new OkHttpClient.Builder().build();
+
+        Request request = new Request.Builder()
+                .url("https://api.imgur.com/3/gallery/user/rising/0.json")
+                .header("Authorization","Client-ID bb0c749c6403fd2")
+                .header("User-Agent","epicture")
+                .build();
+
+        httpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "An error has occurred " + e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
+    }
+
+    private static class Photo {
+        String id;
+        String title;
+    }
 
 }
