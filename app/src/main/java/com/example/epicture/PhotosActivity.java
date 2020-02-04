816dc2c73ec6e05c1744b7be194feda3ef661eec
florian <florian.Cargouet@epitech.eu>
2020-02-04 12:32:38 +0100

    page fetch data and print it

--- app/src/main/java/com/example/epicture/PhotosActivity.java
+++ app/src/main/java/com/example/epicture/PhotosActivity.java
@@ -1,4 +1,127 @@
 package com.example.epicture;
 
-public class PhotosActivity {
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class PhotosActivity extends AppCompatActivity {
+
+    private OkHttpClient httpClient;
+    private static final String TAG = "PhotoActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photos);
+        fetchData();
+    }
+
+    private void fetchData() {
+        httpClient = new OkHttpClient.Builder().build();
+        Request request = new Request.Builder()
+                .url("https://api.imgur.com/3/gallery/user/rising/0.json")
+                .header("Authorization","Client-ID bb0c749c6403fd2")
+                .header("User-Agent","epicture")
+                .build();
+
+        httpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "An error has occurred " + e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    JSONObject data = new JSONObject(response.body().string());
+                    JSONArray items = data.getJSONArray("data");
+                    final List<Photo> photos = new ArrayList<Photo>();
+                    for(int i=0; i<items.length();i++) {
+                        JSONObject item = items.getJSONObject(i);
+                        Photo photo = new Photo();
+                        if(item.getBoolean("is_album")) {
+                            photo.id = item.getString("cover");
+                        } else {
+                            photo.id = item.getString("id");
+                        }
+                        photo.title = item.getString("title");
+                        photos.add(photo);
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                render(photos);
+                            }
+                        });
+                    }
+                }
+                catch (Exception e) {
+                    Log.e("JSONerr" , "Something went wrong.");
+                }
+            }
+        });
+    }
+
+    private static class PhotoVH extends RecyclerView.ViewHolder {
+        ImageView photo;
+        TextView title;
+
+        public PhotoVH(View itemView) {
+            super(itemView);
+        }
+    }
+
+    private void render(final List<Photo> photos) {
+        RecyclerView rv = (RecyclerView)findViewById(R.id.rv_of_photos);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+        RecyclerView.Adapter<PhotoVH> adapter = new RecyclerView.Adapter<PhotoVH>() {
+            @NonNull
+            @Override
+            public PhotoVH onCreateViewHolder(ViewGroup parent, int viewType) {
+                PhotoVH vh = new PhotoVH(getLayoutInflater().inflate(R.layout.item, null));
+                vh.photo = (ImageView) vh.itemView.findViewById(R.id.photo);
+                vh.title = (TextView) vh.itemView.findViewById(R.id.title);
+                return vh;
+            }
+
+            @Override
+            public void onBindViewHolder(PhotoVH holder, int position) {
+                Picasso.with(PhotosActivity.this).load("https://i.imgur.com/" +
+                        photos.get(position).id + ".jpg").into(holder.photo);
+                holder.title.setText(photos.get(position).title);
+            }
+
+            @Override
+            public int getItemCount() {
+                return photos.size();
+            }
+        };
+
+        rv.setAdapter(adapter);
+    }
 }
