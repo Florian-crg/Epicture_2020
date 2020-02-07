79bc4394408d24491001d3e09717cc9cabdd18d1
CalimReo <thibaultvandamme@hotmail.fr>
2020-02-07 11:37:14 +0100

    clean + profil

--- app/src/main/java/com/example/epicture/ProfileActivity.java
+++ app/src/main/java/com/example/epicture/ProfileActivity.java
@@ -4,22 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity  extends AppCompatActivity {
+
     private ImageButton home_btn;
     private ImageButton favorites_btn;
     private ImageButton search_btn;
     private ImageButton profil_btn;
+    private TextView id_profile;
+    private TextView name_profile;
 
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_profile);
+
         this.home_btn = findViewById(R.id.home_button);
         this.favorites_btn = findViewById(R.id.favorites_button);
         this.search_btn = findViewById(R.id.search_button);
         this.profil_btn = findViewById(R.id.profil_button);
+        this.name_profile = findViewById(R.id.name_profile);
+        this.id_profile = findViewById(R.id.id_profile);
+
+
         home_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,5 +64,7 @@ public class ProfileActivity  extends AppCompatActivity {
                 startActivity(next_activity);
             }
         });
+        id_profile.setText(R.string.id_not_found);
+        name_profile.setText(R.string.name_not_found);
     }
 }
