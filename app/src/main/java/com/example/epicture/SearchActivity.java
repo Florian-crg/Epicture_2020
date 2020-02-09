7eb5404e480ce7fbb37f67a4556835712abf5f97
florian <florian.Cargouet@epitech.eu>
2020-02-09 16:29:36 +0100

    ~ Start real time search

--- app/src/main/java/com/example/epicture/SearchActivity.java
+++ app/src/main/java/com/example/epicture/SearchActivity.java
@@ -2,8 +2,13 @@ package com.example.epicture;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,12 +17,40 @@ public class SearchActivity extends AppCompatActivity {
     private ImageButton favorites_btn;
     private ImageButton search_btn;
     private ImageButton profil_btn;
+    private SearchView mSearchView;
+    private TextView mTextView;
 
+    private TextWatcher text = null;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_search);
+        mSearchView = (SearchView) findViewById(R.id.search);
+        mTextView = (TextView) findViewById(R.id.test);
+        Log.d("TAG", "ok");
+        text = new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.d("TAG", "ok");
+                // FIlters();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.d("TAG", mSearchView.getQuery().toString());
+                mTextView.setText(mSearchView.getQuery().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.d("TAG", "ok");
+
+            }
+        };
 
         this.home_btn = findViewById(R.id.home_button);
         this.favorites_btn = findViewById(R.id.favorites_button);
@@ -57,5 +90,8 @@ public class SearchActivity extends AppCompatActivity {
                 startActivity(next_activity);
             }
         });
+//        mSearchView.OnQueryTextListener(text);
     }
+
+
 }
